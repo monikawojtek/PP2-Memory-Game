a62a44534aba2e97b18eb1f314bbe9dc6cd1c903
monikawojtek <>
2023-08-30 05:37:20 +0000

    addHTML Structure and started creating the game in js

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+/* * define some selectors, and the memo’s state:  */
+
+const selectors = {
+    playGroundContainer: document.querySelector('.playGround-container'),
+    playGround: document.querySelector('.playGround'),
+    clock: document.querySelector('.clock'),
+    trials: document.querySelector('.trials'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+};
+
+/** flickedSymbol is used to keep track of the number of flickes of the cards. Only two cards can be flipped at a time. If the symbols tha are flicked are a match, they will bekept symbols up. If it’s a mismatch, they will be flicked back.
+totalFlicks and totalClock will keep track of the total number of tries and the elapsed time since the game started, respectively. */
+
+const state = {
+    memoStarted: false,
+    flickedSymbols: 0,
+    totalFlicks: 0,
+    totalClock: 0,
+    loop: null
+};
+
+
+/** Creating the Memo PlayGround based on the data-dimension attribute. */
+
+const generateMemo = () => {
+    const dimensions = selectors.playGround.getAttribute('data-dimension');
+
+    if (dimensions % 2 !== 0) {
+        throw new Error("The dimension of the pley ground must be an even number.");
+    }
+
+    const animals = ['🐹', '🐩', '🐥', '🐠', '🐌', '🐋', '🐷', '🐢', '🐍', '🐨', '🐓', '🐧', '🐼', '🐑', '🐦', '🐻', '🦎', '🐐', '🦍', '🦁', '🐭', '🐗', '🐜', '🦅', '🦜', '🦝', '🦢', '🦫', '🦉', '🐴', '🐮'];
+    const picks = pickRandom(animals, (dimensions * dimensions) / 2);
+    const items = shuffle([...picks, ...picks]);
+    const symbols = `
+        <div class="playGround" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="symbol">
+                    <div class="symbol-front"></div>
+                    <div class="symbol-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `;
+
+    const parser = new DOMParser().parseFromString(symbols, 'text/html');
+
+    selectors.playGround.replaceWith(parser.querySelector('.playGround'));
+};
+
+
+
+
+
+generateMemo();
